10c2e7136f35f8125e2c7efcf61ff7f0850397a2
Jacksgithubaccoun <158605432+Jacksgithubaccoun@users.noreply.github.com>
2025-09-27 18:52:23 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/jreader/', // <-- IMPORTANT, replace with your repo name
+  base: '/jreader/', // Replace with your repo name
   plugins: [react()],
 })
